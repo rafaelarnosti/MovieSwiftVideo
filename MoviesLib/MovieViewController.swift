d217ee2695d2142e7dfa540dd870314df47510e9
riveregg <rlm.arnosti@gmail.com>
2017-09-29 19:30:19 -0300

    Rafael Commit

---
+++ MoviesLib/MovieViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Eric.
+//  Copyright © 2017 EricBrito. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var ivPoster: UIImageView!
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbGenre: UILabel!
+    @IBOutlet weak var lbDuration: UILabel!
+    @IBOutlet weak var lbScore: UILabel!
+    @IBOutlet weak var tvSinopsis: UITextView!
+    @IBOutlet weak var lcButtonX: NSLayoutConstraint!
+    @IBOutlet weak var viTrailer: UIView!
+    
+    //Variável que receberá o filme selecionado na tabela
+    var movie: Movie!
+    
+    // MARK: Super Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        lbTitle.text = movie.title
+        lbDuration.text = movie.duration
+        lbScore.text = "⭐️ \(movie.rating)/10"
+        tvSinopsis.text = movie.summary
+        if let categories = movie.categories {
+            lbGenre.text = categories.map({($0 as! Category).name!}).joined(separator: " | ")
+        }
+        if let image = movie.poster as? UIImage {
+            ivPoster.image = image
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? MovieRegisterViewController {
+            vc.movie = movie
+        }
+    }
+    
+    //Dessa forma, podemos voltar à tela anterior
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Super Methods
+    @IBAction func playVideo(_ sender: UIButton) {
+    }
+    
+}
